5110609792d3f59bf31a607974a0e3529edc3081
Juan Sierra <sierra.juan99@gmail.com>
2025-06-28 18:13:40 -0700

    initial commit
    - Chapter 1 exercise

---
+++ nature-of-code/bouncing-ball/sketch.js
@@ -0,0 +1,26 @@
+let position;
+let velocity;
+
+function setup() {
+  createCanvas(400, 400);
+  position = createVector(0, 0);
+  velocity = createVector(2.5, 3, 1);
+}
+
+function draw() {
+  background(30);
+  position.add(velocity);
+  stroke(0);
+  fill(100, 100, 255);
+
+  if (position.x >= 400 || position.x <= 0) {
+    velocity.x *= -1;
+  }
+  if (position.y >= 400 || position.y <= 0) {
+    velocity.y *= -1;
+  }
+  if (position.z >= 50 || position.z <= 0){
+    velocity.z *= -1;
+  }
+  circle(position.x, position.y, position.z);
+}
