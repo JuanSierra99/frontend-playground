f535d65bf3d93cbdadc111a3a19e0feef03edc38
Juan Sierra <sierra.juan99@gmail.com>
2025-06-29 20:05:09 -0700

    refactor code
    - utilizing variables and function declarations provided by p5 to resize canvas, and get mouse position

--- nature-of-code/vector-subtraction/sketch.js
+++ nature-of-code/vector-subtraction/sketch.js
@@ -1,33 +1,21 @@
-let width = window.innerWidth;
-let height = window.innerHeight;
-let mouseX = 0;
-let mouseY = 0;
-
-window.addEventListener("resize", () => {
-  width = window.innerWidth;
-  height = window.innerHeight;
-  resizeCanvas(width, height);
-});
-
-window.addEventListener("mousemove", (e) => {
-  mouseX = e.clientX;
-  mouseY = e.clientY;
-});
+function windowResized() {
+  resizeCanvas(windowWidth, windowHeight);
+}
 
 function setup() {
-  createCanvas(width, height);
+  createCanvas(windowWidth, windowHeight);
 }
 
 function draw() {
   background(220);
-  let center = createVector(width / 2, height / 2);
-  let mouse = createVector(mouseX, mouseY)
+  let center = createVector(windowWidth / 2, windowHeight / 2);
+  let mouse = createVector(mouseX, mouseY);
   stroke(150);
-  strokeWeight(4)
+  strokeWeight(4);
   line(0, 0, center.x, center.y);
   line(0, 0, mouse.x, mouse.y);
   mouse.sub(center);
   stroke(0, 0, 255);
-  translate(width / 2, height / 2)
+  translate(windowWidth / 2, windowHeight / 2);
   line(0, 0, mouse.x, mouse.y);
 }
